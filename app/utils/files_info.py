95374f6e0a9ce163762a1712f125f075e37d14fc
AnaPana <admin@it-recipes.com>
2014-03-19 05:25:21 +0200

    Added metafiles and table sorting for them

---
+++ app/utils/files_info.py
@@ -0,0 +1,43 @@
+import os
+import time
+import datetime
+
+from app.utils.log_helper import roots
+
+def get_configs():
+    lst = [
+             ("mfsexports", "mfsexports.cfg"), 
+             ("mfstopology", "mfstopology.cfg"),
+             ("mfsmaster", "mfsmaster.cfg")
+             ]
+    
+    return lst
+
+
+def get_metafiles_info():
+    lst = []
+    path = roots['metafiles']
+    format = "%a %b %d %H:%M:%S %Y"
+    for dirpath, dirnames, filenames in os.walk(path):
+        for file in filenames:
+            (mode, ino, dev, nlink, uid, gid, size, atime, mtime, ctime) = \
+                                                os.stat(os.path.join(path, file))
+            lst.append({'name' : file,
+                        'mode' : oct(mode),
+                        'uid' : uid,
+                        'gid' : gid,
+                        'size' : size,
+                        'atime' : _format_time(atime, format),
+                        'mtime' : _format_time(mtime, format),
+                        'ctime' : _format_time(ctime, format)
+                        })
+    return lst
+
+def _format_time(t, f):
+    return datetime.datetime.strptime(time.ctime(t), f)
+
+def sizeof_fmt(size):
+    for x in ['B','KB','MB','GB','TB']:
+        if size < 1024.0:
+            return "%3.1f %s" % (size, x)
+        size /= 1024.0
--- app/views/master.py
+++ app/views/master.py
@@ -2,29 +2,26 @@ from flask import render_template, redirect, request, url_for
 from app import app
 from app.forms import LoginForm
 from app.decorators import login_required
+from app.utils.files_info import get_configs, get_metafiles_info
+from app.utils.log_helper import roots
 
 import os
 
 @app.route('/master', methods = ['GET', 'POST'])
 @login_required
 def master():
-    fileslist = [
-                 ("mfsexports", "mfsexports.cfg"), 
-                 ("mfstopology", "mfstopology.cfg"),
-                 ("mfsmaster", "mfsmaster.cfg")
-                 ]
-    lst = []
-    for file in fileslist:
-        lst.append(file)
+    configs = get_configs()
+    metafiles = get_metafiles_info()
     return render_template('master.html',
-                           fileslist = lst,
+                           configs = configs,
+                           metafiles = metafiles,
                            title = 'Master')
 
 
 @app.route('/config_editor/<config_name>', methods = ['GET', 'POST'])
 @login_required
 def config_editor(config_name):
-    path = "/etc/mfs"
+    path = roots['configs']
     filename = os.path.join(path, config_name + '.cfg')
     if request.method == 'POST':
         with open(filename, 'w') as f:
