9d17aed4167ad78ae2467a6b694a4b195aa1e23a
AnaPana <admin@it-recipes.com>
2014-03-19 10:38:43 +0200

    directory tree added

--- app/views/main.py
+++ app/views/main.py
@@ -3,6 +3,7 @@ from app import app
 from app.forms import LoginForm
 from app.decorators import login_required
 
+import os
 
 @app.route('/')
 @login_required
@@ -13,5 +14,33 @@ def index():
 @app.route('/home', methods = ['GET', 'POST'])
 @login_required
 def home():
-    return render_template('main.html',
+    tree = make_tree('/usr/local')
+    return render_template('home.html',
+                           tree = tree,
                            title = 'Home')
+
+
+def make_tree(path, id=''):
+    tree = dict(id=id, is_dir=True, name=os.path.basename(path), children=[])
+    try: 
+        lst = os.listdir(path)
+    except OSError:
+        pass #ignore errors
+    else:
+        for name in lst:
+            id = _set_id()
+            fn = os.path.join(path, name)
+            if os.path.isdir(fn):
+                tree['children'].append(make_tree(fn, id))
+            else:
+                tree['is_dir'] = False
+                tree['children'].append(dict(id=id, name=name))
+    return tree
+
+import random
+import string
+
+def _set_id():
+    digits = "".join( [random.choice(string.digits) for i in xrange(8)] )
+    chars = "".join( [random.choice(string.letters) for i in xrange(15)] )
+    return digits + chars
