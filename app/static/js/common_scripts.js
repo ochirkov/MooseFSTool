876d178aac9b8ec01255b6cec8ea53f62d637f0a
AnaPana <admin@it-recipes.com>
2014-04-27 21:15:26 +0300

    Moved all javascript to common-scripts.js

---
+++ app/static/js/common_scripts.js
@@ -0,0 +1,99 @@
+$(document).ready(function() {
+	$(".sorted-table").tablesorter({
+		sortList : [[0, 0]]
+	});
+});
+
+////////////////////
+// CONFIGS EDITOR //
+////////////////////
+
+function edit_request(event, config_name) {
+	(event.preventDefault) ? event.preventDefault() : event.returnValue = false;
+	$.post("", {
+		config_name : config_name,
+		action : 'edit',
+		csrfmiddlewaretoken : getCookie('csrftoken'),
+	}, function(data) {
+		$('#id_content').html(data);
+	});
+}
+
+function confirm_save(event, config_name) {
+	(event.preventDefault) ? event.preventDefault() : event.returnValue = false;
+	var content = $('#id_config_text').val()
+	var root_passwd = $('#id_root_passwd').val()
+	var c = confirm("Do you realy want to save file " + config_name +"?");
+	if (c == true) {
+		$.post("", {
+			config_name : config_name,
+			content : content,
+			root_passwd : root_passwd,
+			action : 'save',
+			csrfmiddlewaretoken : getCookie('csrftoken'),
+		}, function(data) {
+			if (data == '') {
+				window.location.reload();
+			} else {
+				$('#id_error_div').removeClass('hidden')
+				$('#id_error_div').html(data)
+			}
+		});
+	}
+
+}
+
+function confirm_cancel(event) {
+	(event.preventDefault) ? event.preventDefault() : event.returnValue = false;
+	var c = confirm("Do you really want to discard changes? All changes will be lost!");
+	if (c == true) {
+		window.location.reload()
+	}
+}
+
+////////////////
+// FILES TREE //
+////////////////
+
+function open_folder(event, id, full_name) {
+	(event.preventDefault) ? event.preventDefault() : event.returnValue = false;
+	
+	if ($('#' + id).hasClass('in')) {
+		$("#" + id).collapse('hide');
+    	$("#" + id + "_img_minus").addClass('hidden')
+    	$("#" + id + "_img_plus").removeClass('hidden')
+	}
+	
+	else {
+		var onclick = $('#link_' + id).attr('onclick')
+		$('#link_' + id).removeAttr('onclick');
+		$("#loading_img_" + id).removeClass('hidden');
+		$.post("", {
+	        full_name : full_name,
+	        csrfmiddlewaretoken: getCookie('csrftoken'),
+	        },function(data) {
+	        	$('#' + id).html(data);
+	        	$("#" + id).collapse('show');
+	        	$("#" + id + "_img_plus").addClass('hidden')
+    			$("#" + id + "_img_minus").removeClass('hidden')
+    			$("#loading_img_" + id).addClass('hidden')
+    			$('#link_' + id).attr('onclick', onclick)
+	        }
+		);
+	}
+}
+
+function send_post(event, full_name, action, is_dir) {
+	(event.preventDefault) ? event.preventDefault() : event.returnValue = false;
+	$("#loading_info").removeClass('hidden')
+	$.post("/data/info", {
+        full_name : full_name,
+        action : action,
+        is_dir : is_dir,
+        csrfmiddlewaretoken: getCookie('csrftoken'),
+        },function(data) {
+              $('#get_info_div').html(data);
+              $("#loading_info").addClass('hidden')
+        }
+	);
+}
